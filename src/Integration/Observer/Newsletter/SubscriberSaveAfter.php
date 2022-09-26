6b540e643fc6db9eb56d3d8fbee31e361d80db66
Denis_Z <work.zavyalov.d.s@gmail.com>
2022-09-26 15:18:59 +0400

    FLAS-2: Re-factor Export new Magento newsletter subscriber

---
+++ src/Integration/Observer/Newsletter/SubscriberSaveAfter.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types=1);
+
+namespace Flashy\Integration\Observer\Newsletter;
+
+use Flashy\Integration\Helper\Data;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\AddressInterface;
+use Magento\Customer\Model\ResourceModel\CustomerRepositoryFactory;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Newsletter\Model\Subscriber;
+
+class SubscriberSaveAfter implements ObserverInterface
+{
+    private Data $helper;
+    private CustomerRepositoryFactory $customerRepositoryFactory;
+
+    /**
+     * @param Data $helper
+     * @param CustomerRepositoryFactory $customerRepositoryFactory
+     */
+    public function __construct(
+        Data $helper,
+        CustomerRepositoryFactory $customerRepositoryFactory
+    ) {
+        $this->helper = $helper;
+        $this->customerRepositoryFactory = $customerRepositoryFactory;
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @event newsletter_subscriber_save_after
+     *
+     * @param Observer $observer
+     */
+    public function execute(Observer $observer)
+    {
+        if (!$this->helper->getFlashyActive()) {
+            return;
+        }
+
+        /** @var Subscriber $subscriber */
+        $subscriber = $observer->getEvent()->getSubscriber();
+        if ($subscriber->getStatus() !== Subscriber::STATUS_SUBSCRIBED) {
+            return;
+        }
+
+        $subscriberData = [
+            'email' => $subscriber->getEmail(),
+        ];
+
+
+        try {
+            /** @var CustomerRepositoryInterface $customerRepository */
+            $customerRepository = $this->customerRepositoryFactory->create();
+            $customerDataModel = $customerRepository->getById((int)$subscriber->getCustomerId());
+
+            $defaultBillingAddress = null;
+
+            /** @var AddressInterface $address */
+            foreach ($customerDataModel->getAddresses() ?: [] as $address) {
+                if ($address->getId() === $customerDataModel->getDefaultBilling()) {
+                    $defaultBillingAddress = $address;
+                    break;
+                }
+            }
+
+            if ($defaultBillingAddress !== null) {
+                $subscriberData['telephone'] = $defaultBillingAddress->getTelephone();
+                $subscriberData['firstname'] = $defaultBillingAddress->getFirstname();
+                $subscriberData['lastname'] = $defaultBillingAddress->getLastname();
+                $subscriberData['dob'] = $customerDataModel->getDob();
+                $subscriberData['city'] = $defaultBillingAddress->getCity();
+                $subscriberData['street'] = $defaultBillingAddress->getStreet();
+            }
+
+            $this->helper->subscriberSend($subscriberData, $subscriber->getStoreId());
+        } catch (\Exception $e) {
+            // TODO: implement logic to handle Exceptions
+        }
+    }
+}
